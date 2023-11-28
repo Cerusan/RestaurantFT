08725dc7024ae19ce6598d3468889c8def846d8b
Vlad Tastenov <vladtastenov@gmail.com>
2023-11-29 00:01:07 +0500

    Create a project

---
+++ src/main/java/ru/tastenov/Restaurant/controllers/RestaurantController.java
@@ -0,0 +1,9 @@
+package ru.tastenov.Restaurant.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/restaurant")
+public class RestaurantController {
+}
---
+++ src/test/java/ru/tastenov/Restaurant/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tastenov.Restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
